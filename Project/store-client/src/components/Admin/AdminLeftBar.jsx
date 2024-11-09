c5dce8f38a193e1a8f80a01c7ae3c5dba9d91e22
pavani <pavanisri427@gmail.com>
2024-11-09 18:11:49 +0530

    D12:store-client dashboard

--- Project/store-client/src/components/Admin/AdminLeftBar.jsx
+++ Project/store-client/src/components/Admin/AdminLeftBar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 const AdminLeftBar = () => {
+  const Linksdata = {
+    {
+      title: 'Home',
+      path: '/'
+  },
+  {
+      title: 'Products',
+      path: '/products'
+  },
+  {
+      title: 'Contact',
+      path: '/contact'
+  }
+
+  }
   return (
-    <div className='w-full h-full bg-green-500/20 flex justify-center items-center'>AdminLeftBar</div>
+    <div className='w-full h-full shadow-md  shadow-purple-400 flex justify-center items-center'>
+    <div className='h-full w-full flex flex-col'>
+      <div className='h-[10%] w-full  text-purple-500 flex justify-center font-bold text-2xl'>
+        Max Store
+      </div>
+      <div className='h-[80%] w-full flex flex-col items-center'>
+        links
+      </div>
+       </div>
+    </div>
+  
   )
 }
 
 export default AdminLeftBar
+//admin/dashboard
